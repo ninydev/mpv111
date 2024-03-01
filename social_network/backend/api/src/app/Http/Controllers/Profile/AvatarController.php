9124e018d2408590d841f43193fb8d36f1d1e144
Oleksandr Nykytin <keeper@ninydev.com>
2024-03-01 19:24:18 +0200

    Upload File

--- social_network/backend/api/src/app/Http/Controllers/Profile/AvatarController.php
+++ social_network/backend/api/src/app/Http/Controllers/Profile/AvatarController.php
@@ -12,6 +12,16 @@ class AvatarController extends Controller
     {
         $this->middleware ('auth:api');
     }
+
+    /**
+     * Для решения задачи хранения аватарок пользователя мне необходимо:
+     * 1. Сформировать хранилище - место где будут жить файлы
+     * 2. Проверить, пришел ли файл в запросе
+     * 3. Сохранить файл методами фреймворка (Фасадом для файловой структуры)
+     *
+     * @param Request $request
+     * @return \Illuminate\Http\JsonResponse
+     */
     public function __invoke(Request $request)
     {
         // Проверим - прислал ли пользователь файл
@@ -22,13 +32,16 @@ class AvatarController extends Controller
             ], 422);
         }
 
+        // Настроим переменные для хранения файла
         $avatar = $request->file('avatar');
         $user_id = $request->user()->id;
         $path = $user_id . "/avatar.jpg";
 
+        // Сохраним файл
         Storage::disk('avatars')->put($path, $avatar->getContent() );
         $url = Storage::disk('avatars')->url($path);
 
+        // Построим ответ
         return response()->json([
             "result" => "avatar upload",
             "avatar_url" => $url
